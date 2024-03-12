0770d2d3bdd991d7bcda412d0700a6c8f6e5e928
mithileshchellappan <mithilesh1553@gmail.com>
2024-03-12 20:55:06 +0530

    chatbox UI completed

--- src/pages/dashboard/components/chat-box.tsx
+++ src/pages/dashboard/components/chat-box.tsx
@@ -1,18 +1,27 @@
 import { useEffect, useState } from "react";
-import { BeakerIcon } from "@heroicons/react/24/solid";
+import { BeakerIcon, PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import MyIcon from "../../../assets/aiicon.png";
 
 const ChatBox: React.FC = () => {
   const [hideChat, setHideChat] = useState<boolean>(true);
+  const [chatInput, setChatInput] = useState<string>();
+
+  let handleInput = (e: any) => {
+    setChatInput(e.target.value);
+  };
+
+  let _handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      console.log("do validate");
+      setChatInput(" ");
+    }
+  };
 
   return (
     <div className="w-1/3 flex flex-col border bg-dark-background border-x-gray-700 border-top-gray-700 rounded-sm shadow-md  absolute end-0 bottom-0">
       <div className="flex items-center justify-between border-b p-2">
         <div className="flex items-center">
-          <img
-            className="rounded-full w-7 mr-2"
-            src={MyIcon}
-/>
+          <img className="rounded-full w-7 mr-2" src={MyIcon} />
           <div className="pl-2">
             <div className="font-semibold">
               <a className="hover:underline" href="#">
@@ -24,7 +33,7 @@ const ChatBox: React.FC = () => {
         </div>
         <div className="flex gap-4">
           <button
-            className="inline-flex hover:bg-indigo-50 rounded-full p-2"
+            className="inline-flex hover:bg-gray-700 transition rounded-full p-2"
             type="button"
             onClick={() => setHideChat(!hideChat)}
           >
@@ -60,121 +69,69 @@ const ChatBox: React.FC = () => {
       </div>
       {!hideChat && (
         <div className="min-h-96">
-          {/* Chat box area */}
           <div className="flex-1 px-4 py-4 overflow-y-auto">
-            <div className="flex items-center mb-4">
-              <div className="flex-none flex flex-col items-center space-y-1 mr-4">
-                <img
-                  className="rounded-full w-10 h-10"
-                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                />
-                <a href="#" className="block text-xs hover:underline">
-                  John Doe
-                </a>
-              </div>
-              <div className="flex-1 bg-indigo-400 text-white p-2 rounded-lg mb-2 relative">
-                <div>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            {/* TODO - AI RESPONSE HERE */}
+            <>
+              <div className="flex flex-col justify-start items-start mb-4">
+                <div className="flex-none flex justify-center items-center mr-4 pb-2">
+                  <img className="rounded-full w-7 mr-2" src={MyIcon} />
+                  <p className="text-sm">Convex AI Bot</p>
                 </div>
-
-                <div className="absolute left-0 top-1/2 transform -translate-x-1/2 rotate-45 w-2 h-2 bg-indigo-400"></div>
-              </div>
-            </div>
-
-            <div className="flex items-center flex-row-reverse mb-4">
-              <div className="flex-none flex flex-col items-center space-y-1 ml-4">
-                <img
-                  className="rounded-full w-10 h-10"
-                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                />
-                <a href="#" className="block text-xs hover:underline">
-                  Jesse
-                </a>
-              </div>
-              <div className="flex-1 bg-indigo-100 text-gray-800 p-2 rounded-lg mb-2 relative">
-                <div>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem
-                  ipsum dolor sit amet, consectetur adipisicing elit.
+                <div className="flex-1 bg-sky-600 text-white p-2 rounded-lg mb-2 rounded-tl-none">
+                  <div>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  </div>
                 </div>
-
-                <div className="absolute right-0 top-1/2 transform translate-x-1/2 rotate-45 w-2 h-2 bg-indigo-100"></div>
               </div>
-            </div>
+            </>
 
-            <div className="flex items-center mb-4">
-              <div className="flex-none flex flex-col items-center space-y-1 mr-4">
-                <img
-                  className="rounded-full w-10 h-10"
-                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                />
-                <a href="#" className="block text-xs hover:underline">
-                  John Doe
-                </a>
-              </div>
-              <div className="flex-1 bg-indigo-400 text-white p-2 rounded-lg mb-2 relative">
-                <div>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            {/* TODO - USER INPUT HERE */}
+            <>
+              <div className="flex flex-col justify-start items-end mb-4">
+                <div className="flex-none flex justify-center items-center mr-4 pb-2">
+                  <p className="text-sm mr-4">Baalavignesh A</p>
+
+                  <div className="relative w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                    <svg
+                      className="absolute w-8 h-8 text-gray-400 p-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
                 </div>
-
-                <div className="absolute left-0 top-1/2 transform -translate-x-1/2 rotate-45 w-2 h-2 bg-indigo-400"></div>
+                <div className="flex-1 bg-sky-100 p-2 rounded-lg mb-2 rounded-tr-none text-black">
+                  <div>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  </div>
+                </div>
               </div>
-            </div>
+            </>
           </div>
-          {/* Input Area */}
-          <div className="flex items-center border-t p-2">
-            <div>
-              <button
-                className="inline-flex hover:bg-indigo-50 rounded-full p-2"
-                type="button"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-                  />
-                </svg>
-              </button>
-            </div>
 
-            <div className="w-full mx-2">
+          <div className="flex items-center border-t">
+            <label
+              htmlFor="email"
+              className="relative text-gray-400 focus-within:text-gray-600 block w-full"
+            >
+              <PaperAirplaneIcon className="pointer-events-none w-6 h-6 absolute top-3 right-4 text-sky-500 cursor-pointer " />
+
               <input
-                className="w-full rounded-full border border-gray-200"
+                className="w-full rounded-none text-white p-3 border placeholder-white  focus:rounded-none border-gray-700  bg-dark-background  focus:outline-none focus:ring-0 transition duration-700"
                 type="text"
-                value=""
-                placeholder="Aa"
+                value={chatInput}
+                onChange={handleInput}
+                onKeyDown={_handleKeyDown}
+                placeholder="Ask insights about the resume"
                 autoFocus
               />
-            </div>
-
-            <div>
-              <button
-                className="inline-flex hover:bg-indigo-50 rounded-full p-2"
-                type="button"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                  />
-                </svg>
-              </button>
-            </div>
+            </label>
           </div>
         </div>
       )}
