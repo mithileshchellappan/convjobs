7e63f6bce9eff020a2dec9c63c686e135da90b59
mithileshchellappan <mithilesh153@gmail.com>
2024-03-16 23:51:44 +0530

    Add navigation component and vercel.json configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import About from "./pages/about/about";
 import Welcome from "./pages/welcome/welcome";
 import Dashboard from "./pages/dashboard/dashboard";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Login from "./pages/login/login";
 import MyProfile from "./pages/profile/profile";
 
 export default function App() {
   return (
     <Router>
+        <Navigation />
       <Routes>
         <Route path="/login" Component={Login} />
         <Route path="/dashboard" Component={Dashboard} />
@@ -17,5 +18,19 @@ export default function App() {
         {/* <Route path="*" Component={Error} /> */}
       </Routes>
     </Router>
+  );
+}
+
+const Navigation=()=> {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Welcomw</Link></li>
+        <li><Link to="/login">Contact</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/profile">Profile</Link></li>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+      </ul>
+    </nav>
   );
 }
